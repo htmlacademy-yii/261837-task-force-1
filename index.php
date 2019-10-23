6d180f2bfed3e90f0d3f43acff1d3faade3967be
Alexander <borom@inbox.ru>
2019-10-23 21:00:00 +0300

    Создан класс и тест

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+require_once("task.php");
+
+$task = new Task(1, 2, date("Y-m-d"));
+print("Тест класса Task<br>");
+$task->setAction("start");
+$task->setAction("bla-bla-bla");
+print($task->getStatus() . "<br>");
+$task->setAction("create");
+print($task->getStatus() . "<br>");
+$task->setAction("start");
+print($task->getStatus() . "<br>");
+$task->setAction("approve");
+print($task->getStatus() . "<br>");
+$task->setAction("cancel");
+print($task->getStatus() . "<br>");
+$task->setAction("degree");
+print($task->getStatus() . "<br>");
+print_r($task->getStatuses());
+print("<br>");
+print_r($task->getActions());
+print("<br>");
+print($task->getDateEnd() . "<br>");
